3310a2ab3bb3deef35198a7d1c40fd741bd44ea4
qinwei <qinwei@globalegrow.com>
2018-04-11 20:59:38 +0800

    1

--- spring/spring-source/src/main/java/spring/tx/Client.java
+++ spring/spring-source/src/main/java/spring/tx/Client.java
@@ -1,13 +1,22 @@
 package spring.tx;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import spring.entity.Student;
 
 import java.util.ArrayList;
 
 public class Client {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application_context.xml");
+
+        Resource resource = new ClassPathResource("application_context.xml");
+        BeanFactory beanFactory = new XmlBeanFactory(resource);
+
+        System.out.println( beanFactory.getBean("student", Student.class).getAge());
         /*MyUserService userService = applicationContext.getBean(MyUserService.class);
         MyUser myUser = new MyUser();
         myUser.setId(16);
