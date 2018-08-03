ea272bf5399868425a8a00eb6d28d13a37b802e4
qinwei <qinwei@globalegrow.com>
2018-08-03 16:59:58 +0800

    1

--- reactor/demo/src/main/java/com/bestbigbird/App.java
+++ reactor/demo/src/main/java/com/bestbigbird/App.java
@@ -1,13 +1,76 @@
 package com.bestbigbird;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+
+        List<Integer> list=new ArrayList<Integer>();
+        for(int i=0;i<10;i++){
+            list.add(i );
+        }
+        Stream<Integer> stream=  list.stream();
+        Stream<Integer> streamFilter;
+        Stream<Integer> streamMap;
+
+        MyPredicate myPredicate = new MyPredicate();
+        MyFunction myFunction = new MyFunction();
+        MyConsumer myConsumer  = new MyConsumer();
+
+        streamFilter = stream.filter(
+               /* (s) -> {
+                    System.out.println("i am filter");
+                    return s < 5;
+                }*/
+                myPredicate );
+
+
+        streamMap = streamFilter.map(
+                       /* (a) -> {
+                            System.out.println("i am map");
+                            return   a+1;
+                        }*/ myFunction);
+        streamMap.forEach(
+               /* (a) -> {
+                    System.out.println("i am forEach");
+                    System.out.println( a);
+                }*/ myConsumer);
+
+
+    }
+}
+
+class MyPredicate implements Predicate<Integer> {
+    @Override
+    public boolean test(Integer o) {
+        System.out.println("i am filter");
+        return o.intValue() < 5;
+    }
+}
+
+class MyFunction implements  Function<Integer,Integer>{
+    @Override
+    public Integer apply(Integer o) {
+        System.out.println("i am map");
+        return o.intValue()+1;
+    }
+}
+
+class MyConsumer implements Consumer<Integer> {
+    @Override
+    public void accept(Integer o) {
+        System.out.println("i am forEach");
+        System.out.println(o.intValue());
     }
 }
