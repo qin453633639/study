bb4f7def272fdd623f7d28b4e0fc1abaa3f91409
qinwei <453633639@qq.com>
2018-11-12 17:57:13 +0800

    1

---
+++ java/javassist/src/main/java/com/App.java
@@ -0,0 +1,16 @@
+package com;
+
+import javassist.ClassMap;
+import javassist.ClassPool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        ClassMap a ;
+        ClassPool pool = ClassPool.getDefault();
+        System.out.println(1);
+    }
+}
